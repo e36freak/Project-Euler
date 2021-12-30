a87f0251ef6cecabbeefedb70bf53fe112a2fd9d
e36freak <danielmills1@gmail.com>
2021-12-29 23:28:20 -0500

    way more efficient solution for 29

--- 029.c
+++ 029.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #define SIZE 256
+#define MAX SIZE * 10
 
 typedef struct num {
   int digits[SIZE];
@@ -11,7 +12,11 @@ typedef struct num {
 
 typedef struct {
   struct num * start;
-} list_t;
+} bucket_t;
+
+typedef struct {
+  bucket_t * map[MAX];
+} hash_t;
 
 void print(int num[]) {
   int flag = 0;
@@ -43,12 +48,17 @@ int compare(num_t * a, num_t * b) {
   return 0;
 }
 
-void create_list(list_t * list) {
-  list->start = NULL;
+void create_map(hash_t * list) {
+  bucket_t * bucket;
+  for (int i = 0; i < MAX; i++) {
+    bucket = (bucket_t *)malloc(sizeof(bucket_t));
+    list->map[i] = bucket;
+    bucket->start = NULL;
+  }
 }
 
-void clear_list(list_t * list) {
-  num_t * inc = list->start;
+void clear_bucket(bucket_t * bucket) {
+  num_t * inc = bucket->start;
   num_t * prev = NULL;
   
   while (inc != NULL) {
@@ -56,15 +66,34 @@ void clear_list(list_t * list) {
     inc = inc->next;
     free(prev);
   }
+  free(bucket);
 }
   
-int add_num(list_t * list, num_t * num) {
-  num_t * inc = list->start;
+void clear_map(hash_t * list) {
+  for (int i = 0; i < MAX; i++) {
+    clear_bucket(list->map[i]);
+  }
+}
+
+int calc_hash(num_t * num) {
+  int ret = 0;
+  for (int i = 0; i < SIZE; i++) {
+    ret += num->digits[i];
+  }
+  return ret;
+}
+
+int add_num(hash_t * list, num_t * num) {
+  num_t * inc = NULL;
   num_t * prev = NULL;
+  int hash;
+
+  hash = calc_hash(num);
+  inc = list->map[hash]->start;
 
   num->next = NULL;
   if (inc == NULL) {
-    list->start = num;
+    list->map[hash]->start = num;
     return 1;
   }
   while (inc != NULL && (compare(num, inc) < 0)) {
@@ -80,7 +109,7 @@ int add_num(list_t * list, num_t * num) {
     return 0;
   }
   if (prev == NULL) {
-    list->start = num;
+    list->map[hash]->start = num;
     num->next = inc;
     return 1;
   }
@@ -89,51 +118,49 @@ int add_num(list_t * list, num_t * num) {
   return 1;
 }
 
-void power(int a, int b, int res[]) {
-  int i, j, t, r;
-  int tmp[SIZE];
-  t = a;
+void mult(int a[], int b, int res[]) {
+  int t, i, j, r;
   for (i = 0; i < SIZE; i++) {
-    tmp[i] = t % 10;
-    t /= 10;
+    res[i] = 0;
   }
-  for (int p = 1; p < b; p++) {
-    for (i = 0; i < SIZE; i++) {
-      res[i] = 0;
-    }
-    for (i = 0; i < SIZE; i++) {
-      t = tmp[i] * a;
-      j = 0;
-      while (t > 0) {
-        res[i + j] += t % 10;
-        if ((r = res[i + j]) >= 10) {
-          res[i + j] = r % 10;
-          res[i + j + 1] += r / 10;
-        }
-        t /= 10;
-        j++;
+  for (i = 0; i < SIZE; i++) {
+    t = a[i] * b;
+    j = 0;
+    while (t > 0) {
+      res[i + j] += t % 10;
+      if ((r = res[i + j]) >= 10) {
+        res[i + j] = r % 10;
+        res[i + j + 1] += r / 10;
       }
+      t /= 10;
+      j++;
     }
-    arr_cpy(tmp, res);
   }
 }
 
 int main(void) {
   num_t * tmp;
-  list_t powers;
+  hash_t powers;
   int ans = 0;
+  int num[SIZE], t;
 
-  create_list(&powers);
+  create_map(&powers);
 
   for (int a = 2; a <= 100; a++) {
+    t = a;
+    for (int i = 0; i < SIZE; i++) {
+      num[i] = t % 10;
+      t /= 10;
+    }
     for (int b = 2; b <= 100; b++) {
       tmp = (num_t *)malloc(sizeof(num_t));
-      power(a, b, tmp->digits);
+      mult(num, a, tmp->digits);
+      arr_cpy(num, tmp->digits);
       if (add_num(&powers, tmp)) {
         ans++;
       }
     }
   }
   printf("%d\n", ans);
-  clear_list(&powers);
+  clear_map(&powers);
 }
